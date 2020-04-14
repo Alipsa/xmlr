8692b349ec8852a7d22dc4775b6ae84bdb7e5d70
perNyfelt <Tian1Pan>
2020-04-14 12:33:16 +0200

    Add examples, Mention upwards navigation in vignette

--- R/xmlr.R
+++ R/xmlr.R
@@ -4,7 +4,28 @@
 #' @import methods
 #' @docType package
 #' @name xmlr
-# @examples
-# doc <- Document$new()
+#' @examples
+#' library("xmlr")
+#' doc <- Document$new()
+#' root <- Element$new("table")
+#' root$setAttribute("xmlns", "http://www.w3.org/TR/html4/")
+#' doc$setRootElement(root)
+#'
+#' root$addContent(
+#'   Element$new("tr")
+#'     $addContent(Element$new("td")$setText("Apples"))
+#'     $addContent(Element$new("td")$setText("Bananas"))
+#' )
+#' table <- doc$getRootElement()
+#' stopifnot(table$getName() == "table")
+#' stopifnot(table$getAttribute("xmlns") == "http://www.w3.org/TR/html4/")
+#'
+#' children <- table$getChild("tr")$getChildren()
+#' stopifnot(length(children) == 2)
+#' stopifnot(children[[1]]$getText() == "Apples")
+#' stopifnot(children[[2]]$getText() == "Bananas")
+#'
+#' # you can also parse character strings (or parse a file using parse.xmlfile(fileName))
+#' doc <- parse.xmlstring("<foo><bar><baz val='the baz attribute'/></bar></foo>")
 
 NULL
